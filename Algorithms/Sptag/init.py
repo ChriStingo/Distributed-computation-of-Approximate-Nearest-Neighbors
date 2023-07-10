e984b537cc5e8a02cfa60bbc8b5605df8a7bf92d
christingo <stingone.christian@gmail.com>
2023-07-10 12:35:24 +0200

    testing solution

--- Algorithms/Sptag/init.py
+++ Algorithms/Sptag/init.py
@@ -6,13 +6,9 @@ from os import listdir
 from config import DATASETS_IN_RAM, DEBUG, DISTANCE, NUMBER_OF_THREADS, PATH_INDEX, PATH_DATASETS, get_dataset_columns
 
 def create_sptag_index():
-    index_type = SPTAG.IndexAlgo.SPANN  # Tipo di indice SPANN
-    distance_type = SPTAG.DistanceType.Cosine  # Tipo di distanza Cosine
-    index_builder = SPTAG.IndexBuilder()
-    index_builder.SetNumThreads(NUMBER_OF_THREADS)
-    index_builder.SetIndexAlgorithm(index_type)
-    index_builder.SetDistanceType(distance_type)
-    sptag_index = index_builder.Build(float, get_dataset_columns())
+    sptag_index = SPTAG.AnnIndex('BKT', 'Float', get_dataset_columns())
+    sptag_index.SetBuildParam("NumberOfThreads", NUMBER_OF_THREADS, "Index")
+    sptag_index.SetBuildParam("DistCalcMethod", DISTANCE, "Index") 
     return sptag_index
     
 def train_index(sptag_index):
@@ -28,27 +24,27 @@ def train_index(sptag_index):
                 matrix.append(np.array(vector).astype(np.float32))
                 
     sptag_index.Build(np.asmatrix(matrix).astype(np.float32), len(matrix), False)
-    sptag_index.AddWithIds(np.asmatrix(matrix).astype(np.float32), np.arange(len(matrix)))
+    sptag_index.Add(np.asmatrix(matrix).astype(np.float32), len(matrix), False)
 
 def fill_index(sptag_index):
     # Read each dataset in the folder and insert its vectors in the index
-    for dataset_name in sorted(listdir(PATH_DATASETS))[0:]:
+    for dataset_name in sorted(listdir(PATH_DATASETS))[DATASETS_IN_RAM:]:
         with open(PATH_DATASETS + dataset_name, "r") as dataset:
             datareader = csv.reader(dataset)
             DEBUG(['Loading', dataset_name])
             
             # Add to index
+            matrix = []
             for vector in datareader:
-                to_add = np.asmatrix(np.array(vector).astype(np.float32))
-                sptag_index.AddWithIds(to_add, np.arange(len(to_add)))
+                matrix.append(np.array(vector).astype(np.float32))
+            sptag_index.Add(np.asmatrix(matrix).astype(np.float32), len(matrix), False)
 
 def build_and_save_sptag_index(sptag_index):
-    sptag_index.Build()
-    sptag_index.SaveIndex(PATH_INDEX)
+    sptag_index.Save(PATH_INDEX)
 
 def main():
     sptag_index = create_sptag_index()
-    #train_index(sptag_index)
+    train_index(sptag_index)
     fill_index(sptag_index)
     build_and_save_sptag_index(sptag_index)
 
