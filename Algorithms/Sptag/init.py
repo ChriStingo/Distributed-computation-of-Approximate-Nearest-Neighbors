7650852e64e2b8a07948a38d9a57eab5c01afef1
christingo <stingone.christian@gmail.com>
2023-07-10 12:05:26 +0200

    testing some solutions

--- Algorithms/Sptag/init.py
+++ Algorithms/Sptag/init.py
@@ -6,11 +6,11 @@ from os import listdir
 from config import DATASETS_IN_RAM, DEBUG, DISTANCE, NUMBER_OF_THREADS, PATH_INDEX, PATH_DATASETS, get_dataset_columns
 
 def create_sptag_index():
-    sptag_index = SPTAG.AnnIndex('SPANN', 'Float', get_dataset_columns())
+    sptag_index = SPTAG.AnnIndex('BKT', 'Float', get_dataset_columns())
     # Set the thread number to speed up the build procedure in parallel 
     sptag_index.SetBuildParam("NumberOfThreads", NUMBER_OF_THREADS, "Index")
     # Set the distance type. Currently SPTAG only support Cosine and L2 distances. Here Cosine distance is not the Cosine similarity. The smaller Cosine distance it is, the better.
-    sptag_index.SetBuildParam("DistCalcMethod", DISTANCE, "Index") 
+    sptag_index.SetBuildParam("DistCalcMethod", "L2", "Index") 
     return sptag_index
     
 def train_index(sptag_index):
@@ -25,10 +25,9 @@ def train_index(sptag_index):
             for vector in datareader:
                 matrix.append(np.array(vector).astype(np.float32))
                 
-    if sptag_index.Build(np.asmatrix(matrix), len(matrix), False):
-        print("build e save")
-        sptag_index.Save(PATH_INDEX)
-    #sptag_index.Add(np.asmatrix(matrix), len(matrix), False)
+    print(np.asmatrix(matrix).astype(np.float32).shape, len(matrix))
+    sptag_index.Build(np.asmatrix(matrix).astype(np.float32), len(matrix), False)
+    sptag_index.Add(np.asmatrix(matrix).astype(np.float32), len(matrix), False)
 
 def fill_index(sptag_index):
     # Read each dataset in the folder and insert its vectors in the index
@@ -39,7 +38,7 @@ def fill_index(sptag_index):
             
             # Add to index
             for vector in datareader:
-                to_add = np.asmatrix(np.array(vector).astype(np.longdouble))
+                to_add = np.asmatrix(np.array(vector).astype(np.float32))
                 sptag_index.Add(to_add, len(to_add), False)
 
 def build_and_save_sptag_index(sptag_index):
@@ -48,8 +47,8 @@ def build_and_save_sptag_index(sptag_index):
 def main():
     sptag_index = create_sptag_index()
     train_index(sptag_index)
-    #fill_index(sptag_index)
-    #build_and_save_sptag_index(sptag_index)
+    fill_index(sptag_index)
+    build_and_save_sptag_index(sptag_index)
 
 if __name__ == "__main__":
     main()
