bc2f7dd68aa63e9b2ad112c7f1cb3c466825f967
christingo <stingone.christian@gmail.com>
2023-09-26 10:31:53 +0200

    refactor formatDatasets

--- Datasets/formatDatasets.bash
+++ Datasets/formatDatasets.bash
@@ -1,15 +1,15 @@
 mkdir -p Indexes/
+mkdir -p Images/
+mkdir -p Vectors/
+
 rm -rf Images/images.txt
-if [ ! -d ./Vectors/ ]
-    then mkdir ./Vectors/
-fi
-if [ ! -d ./Images/ ]
-    then mkdir ./Images/
-fi
+
 cd Decompressed/
 for i in *; 
 do 
     echo 'get filtered data from '$i;
     cut -f2- $i > ../Vectors/$i;
     echo 'get images links from '$i;
-    sed 's/|/ /' $i | awk '{print $1}' >> ../Images/images.txt; done
+    sed 's/|/ /' $i | awk '{print $1}' >> ../Images/images.txt; 
+    rm $i; 
+done
