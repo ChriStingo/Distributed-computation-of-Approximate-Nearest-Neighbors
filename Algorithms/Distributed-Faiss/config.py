4632b52baa16c7d14bf2cd28af7cf45f5fee7bca
christingo <stingone.christian@gmail.com>
2023-10-24 11:27:20 +0200

    some distributed faiss fixes

--- Algorithms/Distributed-Faiss/config.py
+++ Algorithms/Distributed-Faiss/config.py
@@ -5,12 +5,12 @@ PATH_IMAGES = '../../Datasets/Images/images.txt'
 
 
 ''' ### INDEX CONFIG ### '''
-DISTANCE = "l2"
+DISTANCE = "dot"
 INDEX_ID = "images"
 TRAIN_RATIO = 1.0
 INDEX_ALREADY_CREATED = False
 INDEX_TYPE = None
-INDEX_FACTORY = "IMI2x2,PQ2048x4fs,RFlat"
+INDEX_FACTORY = "IVF256,PQ2048x4fs,RFlat"
 
 
 ''' ### UTILS ### '''
--- Algorithms/Distributed-Faiss/init_server.py
+++ Algorithms/Distributed-Faiss/init_server.py
@@ -1,4 +1,4 @@
-from config import INDEX_ALREADY_CREATED, PATH_DATASETS
+from config import INDEX_ALREADY_CREATED, PATH_INDEX
 from distributed_faiss.server import IndexServer
 
 ''' ### SINGLE SERVER CONFIG ### '''
@@ -6,7 +6,7 @@ SERVER_ID = 0       # different for each server
 SERVER_PORT = 8081
 
 def main():
-    server = IndexServer(SERVER_ID, PATH_DATASETS)
+    server = IndexServer(SERVER_ID, PATH_INDEX)
     server.start_blocking(SERVER_PORT, load_index=INDEX_ALREADY_CREATED)
 
 if __name__ == "__main__":
